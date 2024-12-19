9644eec7f82fb4c32e4da3c59e23f1aecd86fe09
guifroes1984 <guilhermefroes26@gmail.com>
2024-12-19 00:02:28 -0300

    Criando o service para registar Cliente e Empresa.

--- src/app/basico/services/auth/auth.service.ts
+++ src/app/basico/services/auth/auth.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASIC_URL = 'http://localhost:8080/'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  registrarCliente(inscreverRequestDTO: any): Observable<any> {
+    return this.http.post(BASIC_URL + "cliente/inscrever", inscreverRequestDTO);
+  }
+
+  registrarEmpresa(inscreverRequestDTO: any): Observable<any> {
+    return this.http.post(BASIC_URL + "empresa/inscrever", inscreverRequestDTO);
+  }
+
 }
