b19a80c634e0e4f67fef67d30e88e5f26187109c
guifroes1984 <guilhermefroes26@gmail.com>
2025-01-06 23:10:57 -0300

    Criando página de login/ Componentes com chamada de API em Angular.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InscreverClienteComponent } from './basico/componentes/inscrever-cliente/inscrever-cliente.component';
 import { InscreverEmpresaComponent } from './basico/componentes/inscrever-empresa/inscrever-empresa.component';
+import { LoginComponent } from './basico/componentes/login/login.component';
 
 const routes: Routes = [
   { path: 'registrar_cliente', component: InscreverClienteComponent},
   { path: 'registrar_empresa', component: InscreverEmpresaComponent},
+  { path: 'login', component: LoginComponent},
   { path: 'empresa', loadChildren: () => import('./empresa/empresa.module').then(m => m.EmpresaModule) }, 
   { path: 'cliente', loadChildren: () => import('./cliente/cliente.module').then(m => m.ClienteModule) }];
 
--- src/app/basico/componentes/login/login.component.ts
+++ src/app/basico/componentes/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  validaForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder, 
+    private authService: AuthService, 
+    private notification: NzNotificationService, 
+    private router: Router) { 
+  }
+  
+  ngOnInit() {
+    this.validaForm = this.formBuilder.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]]
+    })
+  }
+
+  submeterForm() {
+    this.authService.login(this.validaForm.get(['userName'])!.value, this.validaForm.get(['password'])!.value)
+    .subscribe(res => {
+      console.log(res)
+    }, error =>{
+      this.notification
+      .error(
+        'ERRO', 
+        'Credenciais incorretas',
+        {nzDuration: 5000}
+      )
+    })
+  }
 
 }
